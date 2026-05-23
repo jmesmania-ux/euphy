66d70d26122157b8e728577a867b9592646195ef
jmesmania-ux <jmesmania@gmail.com>
2026-05-23 20:40:22 +0800

    Update direction comment in message schema

--- models/Message.js
+++ models/Message.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
-
 const messageSchema = new mongoose.Schema({
   senderId: String,
   senderName: String,
   receiverId: String,
   text: String,
-  direction: String, // 'to_admin' or 'to_customer'
+  direction: String, // 'customer_to_admin' | 'admin_to_customer'
   read: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }
 });
-
 module.exports = mongoose.model('Message', messageSchema);

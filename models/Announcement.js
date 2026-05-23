5989826bd9d57e7b63a6d332308dbdac14972d56
jmesmania-ux <jmesmania@gmail.com>
2026-05-23 20:38:54 +0800

    Remove unnecessary whitespace in Announcement.js

--- models/Announcement.js
+++ models/Announcement.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
-
 const announcementSchema = new mongoose.Schema({
   text: String,
   active: { type: Boolean, default: true },
   createdAt: { type: Date, default: Date.now }
 });
-
 module.exports = mongoose.model('Announcement', announcementSchema);

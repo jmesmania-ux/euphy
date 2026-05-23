173e1ff08b6cda6d86ebc3d44244f291cc6802c9
jmesmania-ux <jmesmania@gmail.com>
2026-05-23 20:58:38 +0800

    Update Announcement.js

--- models/Announcement.js
+++ models/Announcement.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const announcementSchema = new mongoose.Schema({
   text: String,
+  imageUrl: String, // 🆕 Added for images
   active: { type: Boolean, default: true },
   createdAt: { type: Date, default: Date.now }
 });
